4a764b7930164633b5ac386135c60008178aadd5
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-02-21 10:24:24 +0000

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 	let { children } = $props();
 </script>
 
