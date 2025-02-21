c00a2529ec1be74c108d81a86c5405a1d4784f4b
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-02-21 10:24:05 +0000

    problems as link

--- src/routes/problems/+page.svelte
+++ src/routes/problems/+page.svelte
@@ -5,21 +5,24 @@
 <main
 	class="bg-background border-border mx-auto mt-6 flex max-w-screen-xl flex-col gap-y-4 rounded-lg border p-3"
 >
-	<div class="flex flex-col gap-3 lg:flex-row">
+	<a
+		class="flex flex-col gap-3 lg:flex-row"
+		href="https://ceobs.org/ukraine-conflict-environmental-briefing-the-climate-crisis/"
+	>
 		<img class="rounded-md" src="/reports/impact.webp" alt="Ukrainian War Impact" />
 		<div class="flex flex-col">
 			<span class="text-2xl">{messages.briefing_title()}</span>
 			<p class="text-muted-foreground">{messages.briefing_description()}</p>
 		</div>
-	</div>
+	</a>
 
 	<hr />
 
-	<div class="flex flex-col gap-3 lg:flex-row">
+	<a class="flex flex-col gap-3 lg:flex-row" href="https://doi.org/10.1186/s12995-023-00398-y">
 		<img class="rounded-md" src="/reports/marine.webp" alt="Ukrainian War Impact" />
 		<div class="flex flex-col">
 			<span class="text-2xl">{messages.marine_title()}</span>
 			<p class="text-muted-foreground">{messages.marine_description()}</p>
 		</div>
-	</div>
+	</a>
 </main>
